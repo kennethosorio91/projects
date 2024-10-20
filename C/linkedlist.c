9a2512c0a4b86ec5e457838ec48f0294bac74bf2
kriezer <kennethposorio@iskolarngbayan.pup.edu.ph>
2024-10-21 00:18:39 +0800

    linkedlist singly insertAtBeginning

--- C/linkedlist.c
+++ C/linkedlist.c
@@ -5,3 +5,48 @@ struct node {
     int data;
     struct node *next;
 };
+
+struct node* createNode(int new_data) {
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+
+    newNode->data = new_data;
+
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void insertAtBeginning(node **head, int new_data) {
+    struct node *newNode = createNode(new_data);
+
+    if(head == NULL) {
+        *head = newNode;
+    }
+    else {
+        newNode->next = *head;
+        *head = newNode;
+    }
+
+}
+
+void printList(struct node *head) {
+    struct node *temp = head;
+
+    while (temp != NULL) {
+        printf(" %d", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct node *head = NULL;
+
+    insertAtBeginning(&head, 15);
+    insertAtBeginning(&head, 10);
+    insertAtBeginning(&head, 5);
+    insertAtBeginning(&head, 1);
+    printList(head);
+
+    return 0;
+}
